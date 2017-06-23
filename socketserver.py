7a17716cc27935f8cf342e6298f04bed4d671f92
SkiaBr23 <rafaelcosta.rdc@hotmail.com>
2017-06-23 20:44:31 -0300

    Testes de sincronismo e processo zumbi

---
+++ socketserver.py
@@ -0,0 +1,109 @@
+import socket
+import thread
+
+class SocketServer(socket.socket):
+    clients = []
+
+    def __init__(self):
+        socket.socket.__init__(self)
+        #To silence- address occupied!!
+        self.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.bind(('127.0.0.1', 4545))
+        self.listen(5)
+
+    def run(self):
+        print "Server started"
+        try:
+            self.accept_clients()
+        except Exception as ex:
+            print ex
+        finally:
+            print "Server closed"
+            for client in self.clients:
+                client.close()
+            self.close()
+
+    def accept_clients(self):
+        while 1:
+            (clientsocket, address) = self.accept()
+            #Adding client to clients list
+            self.clients.append(clientsocket)
+            #Client Connected
+            self.onopen(clientsocket,address)
+            #Receiving data from client
+            thread.start_new_thread(self.recieve, (clientsocket,))
+
+    def recieve(self, client):
+        while 1:
+            data = client.recv(1024)
+            data2 = data.rstrip()
+            if data2 == "list":
+                self.listzombies()
+            if data2 == "die":
+                break
+            #Message Received
+            self.onmessage(client, data)
+        #Removing client from clients list
+        self.clients.remove(client)
+        #Client Disconnected
+        self.onclose(client)
+        #Closing connection with client
+        client.close()
+        #Closing thread
+        thread.exit()
+        print self.clients
+
+    def broadcast(self, message):
+        #Sending message to all clients
+        for client in self.clients:
+            client.send(message)
+
+    def listzombies(self):
+        pass
+
+    def multicast(self,message,sender):
+        #Sending message to all clients but sender
+        for client in self.clients:
+            if client != sender:
+                client.send(message)
+
+    def onopen(self, client, address):
+        pass
+
+    def onmessage(self, client, message):
+        pass
+
+    def onclose(self, client):
+        pass
+
+
+
+class BasicChatServer(SocketServer):
+
+    def __init__(self):
+        SocketServer.__init__(self)
+
+    def onmessage(self, client, message):
+        print "Client Sent Message"
+        #Sending message to all clients
+        self.multicast(message, client)
+
+    def onopen(self, client, address):
+        print "Client Connected = " + str(address)
+
+    def onclose(self, client):
+        print "Client Disconnected"
+
+    def listzombies(self):
+        #List all clients currently connected
+        i = 0
+        for client in self.clients:
+            print "Zombie #" + str(i) + " - at " + client.getsockname()[0]
+            i += 1
+
+def main():
+    server = BasicChatServer()
+    server.run()
+
+if __name__ == "__main__":
+    main()
---
+++ zombie.py
@@ -0,0 +1,17 @@
+from os import fork, getppid, wait
+from sys import exit
+from time import sleep
+
+#comando para ver processos: ps -eo pid,ppid,stat,cmd
+#comando mais simples: top
+
+pid = fork()
+
+if pid == 0:
+    exit("Child: Goodbye, cruel world")
+else:
+    print "Parent: I created a child with pid", pid,\
+          "and now all I want to do is sleep..."
+    while True:
+        print "Caraio borracha"
+        sleep(1)
